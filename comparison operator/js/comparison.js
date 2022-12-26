bf08899ca703084282868930a7e9467ab0d4fcfe
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 21:15:05 +0530

    comparison operator.

---
+++ comparison operator/js/comparison.js
@@ -0,0 +1,13 @@
+//comparison operaetor
+
+var x = parseInt(prompt("enter the value of x"));
+var y = parseInt(prompt("enter the value of y"));
+//var z = x==y
+var z = x!=y;
+var z = x===y;
+var z = x!==y;
+var z = x>=y;
+var z = x>y;
+var z = x<=y;
+var z = x<y;*/
+alert (z);
